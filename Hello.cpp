ee2aeb7d1844e01fea8445c2e9d91b2e43509c71
Shivam93294Valand <147402134+Shivam93294Valand@users.noreply.github.com>
2024-09-01 21:08:29 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// using namespace std;
+using std::cout;
+using std::endl;
+int main() {
+    // std::cout << "Hello World From cpp" << std::endl;
+    cout << "Hello World From cpp" << endl;
+    return 0;
+}
